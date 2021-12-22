108f215037adf5b40ae2d07242636f775248aea7
mina-beiki <mina_beiki@yahoo.com>
2021-12-22 12:22:26 +0330

    init

---
+++ cmake-build-debug/CMakeFiles/bankers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bankers.dir/main.c.o"
+  "bankers"
+  "bankers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bankers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bankers.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bankers.
+# This may be replaced when dependencies are built.
